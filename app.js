53c23422c0f460c03307db65ca5f75d28060541f
Sagar Kashyap <sagarkashyap.cc@gmail.com>
2022-12-19 02:47:11 +0530

    getting cell values in an array

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const puzzleBoard=document.getElementById('puzzle');
 const solveButton=document.getElementById('solve-button');
-
+const submission=[];
 const squares=81;
 
 for(let i=0;i<squares;i++)
 {
     const inputElement=document.createElement('input')
     inputElement.setAttribute('type','number')
-    inputElement.setAttribute('min',0)
+    inputElement.setAttribute('min',1)
     inputElement.setAttribute('max',9)
     puzzleBoard.appendChild(inputElement)
 }
+
+const joinValues=()=>{
+    const inputs=document.querySelectorAll('input')
+    
+    inputs.forEach(input => {
+        if(input.value){
+            submission.push(input.value)
+        }
+        else{
+            submission.push('.')
+        }
+    });
+    console.log(submission)
+}
+
+solveButton.addEventListener('click',joinValues)
